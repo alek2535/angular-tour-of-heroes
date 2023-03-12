b915a73e3440c317bc3db42705afeb895dfd21b3
Alek Valencia <alekvalencia2535@gmail.com>
2023-03-11 17:20:53 -0800

    finished section 5.Add navigation of the tutorial.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,12 +10,10 @@ import { MessageService } from '../message.service';
 })
 
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
   ) { };
 
   ngOnInit(): void {
@@ -26,10 +23,5 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  };
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   };
 };
